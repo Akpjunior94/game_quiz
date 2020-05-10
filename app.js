56798e020f603e6edf974794f1eae517650beddb
Akpan Abraham <Akpjunio4real@gmail.com>
2020-05-10 15:32:25 -0700

    new files and more questions added

--- app.js
+++ app.js
@@ -23,6 +23,29 @@ let questions = [
   },
 
   {
+    question: 'Which one of the following sedimentary basin is related to extension?',
+    options: ['Half-graben', 'Foredeep', 'fore-arc', 'piggyback'],
+    answer: 0
+  },
+
+  {
+    question: 'In seismic session, paraconformity is marked by?',
+    options: ['onlap', 'downlap', 'erosional-truncation', 'concordance'],
+    answer: 3
+  },
+
+  {
+    question: 'To use flexbox we define a container as a flexbox. What is the CSS to do this?',
+    options: [
+      'position:absolute;',
+      'float:left;', 
+      'display:flex;', 
+      'display:block;'
+    ],
+    answer: 2
+  },
+
+  {
     question: 'What is the array prototype method that changes the contents of an array by removing existing elements and/or adding new elements?',
     options: ['[].remove()', '[].slice()', '[].splice()', '[].delete()'],
     answer: 2
@@ -50,11 +73,99 @@ let questions = [
     options: ['‘’', 'Error', 'undefined', 'null'],
     answer: 3
   },
-]
+
+  {
+    question: 'The shape of the earth is best describe as?',
+    options: ['spheriod', 'prolate ellipsoid', 'ellipsoid', 'oblate spheriod'],
+    answer: 3
+  },
+
+  {
+    question: 'Which ine amongst the following is the CORRECT attitude of a bed?',
+    options: ['21°, 95°', 'N45°W, 40°SE', ' 090°/20°W', '089°, 75°S'],
+    answer: 3 
+  },
+
+  {
+    question: 'In which one of the following configurations are the electrodes uniformly spaced?',
+    options: ['Schlumberger array', 'Pole-Dipole array', ' Wenner array', 'Pole-pole array'],
+    answer: 2
+  },
+
+  {
+    question: 'In triclinic crystal system, the three crystallographic axes a,b,c are of?',
+    options: [
+      'Equal lengths with angle between b and c as 90°', 
+      'Equal lengths with angle between a and c not equal 90°', 
+      'Unequal lengths with angle between a and c not equal 90°', 
+      'Unequal lengths with angle between b and c as 90°'
+    ],
+    answer: 2
+  },
+
+  {
+    question: 'Hawaiian Island chain is the result of?',
+    options: [
+      'collision of two oceanic plate', 
+      'intraplate hot spot activity', 
+      'divergence of two oceanic plate', 
+      'interplate hot spot activity'
+    ],
+    answer: 1 
+  },
+
+  {
+    question: 'A land fall that results from free fall of rocks is called?',
+    options: ['talus slope', 'eskers', 'alluvial fan', 'debris flow'],
+    answer: 0 
+  },
+
+  {
+    question: 'Which of the following logging methods is NOT used to determine porosity?',
+    options: ['Sonic', 'SP', 'Neutron', 'Gamma-gamma'],
+    answer: 1
+  },
+
+  {
+    question: `Gardner's formula relates the seismic P-waves to?`,
+    options: ['Permeability', 'Lithology', 'Porosity', 'Density'],
+    answer: 3
+  },
+
+  {
+    question: 'PcP and ScS phases are reflected from?',
+    options: [
+      'Crust - mantle boundary', 
+      'Core - mantle boundary', 
+      'Inner core - outer core boundary', 
+      'Lithosphere - asthenosphere boundary'
+    ],
+    answer: 1
+  },
+
+  {
+    question: 'Identify the CORRECT sequence of the electromagnetic waves in their increasing frequency',
+    options: [
+      'Radiowave, microwave, infrared, visible, ultra violet, X-ray', 
+      'Radiowave, infrared, microwave, visible, ultra violet, X-ray', 
+      'Radiowave, microwave, X-ray, infrared, visible, ultra violet ', 
+      'infrared, Radiowave, microwave, ultra violet, visible, X-ray'
+    ],
+    answer: 0
+  }
+]    
 
 // constants
 const CORRECT_SCORES = 20;
 const MAX_QUESTIONS = 5;
+
+
+const startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions]
+  getNewQuestion();
+};
 
 const getNewQuestion = () => {
   if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
@@ -130,14 +241,6 @@ nextButton.addEventListener('click', e => {
   selectedOption.classList.remove('child-color');
   getNewQuestion()
 })
-
-
-const startGame = () => {
-  questionCounter = 0;
-  score = 0;
-  availableQuestions = [...questions]
-  getNewQuestion();
-};
 
 startGame();
 
